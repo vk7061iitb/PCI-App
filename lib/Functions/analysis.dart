8c11f52bab633d97852101785b44e2f153607597
Vikash Kumar <vk4097440@gmail.com>
2024-04-06 11:38:24 +0530

    function to filter acc data base on timestamp

---
+++ lib/Functions/analysis.dart
@@ -0,0 +1,19 @@
+import '../Objects/data_points.dart';
+
+List<AccData> correctSamplingRate(List<AccData> accData) {
+  List<AccData> correctedAccData = [];
+  correctedAccData.add(accData[0]); // Add the first data point
+
+  for (int i = 1; i < accData.length; i++) {
+    double timeDifference = accData[i]
+        .accTime
+        .difference(correctedAccData.last.accTime)
+        .inMilliseconds
+        .toDouble();
+    if (timeDifference >= 15) {
+      correctedAccData.add(accData[i]);
+    }
+  }
+
+  return correctedAccData;
+}
