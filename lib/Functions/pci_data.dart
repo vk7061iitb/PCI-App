33d5a2068313b25d5619b31427e8fa240c7b8535
Vikash Kumar <vk4097440@gmail.com>
2024-05-04 14:55:16 +0530

    object to store ML output csv data

--- lib/Functions/pci_data.dart
+++ lib/Functions/pci_data.dart
@@ -1,39 +1,33 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import '../Objects/pci_object.dart';
 
 String convertToFirstJsonFormat2(String fileContent) {
-  // Parse the JSON data
   dynamic inputData = jsonDecode(fileContent);
-
-  // Initialize an empty list to store features
   List<Map<String, dynamic>> features = [];
 
-  // Iterate through the provided data
   for (int i = 0; i < inputData.length - 1; i++) {
-    // Extract data for current and next points
     var currentData = inputData[i];
     var nextData = inputData[i + 1];
-
-    // Calculate average velocity and max pci
     double avgVelocity = (currentData['Velocity'] + nextData['Velocity']) / 2;
-    int maxPci = currentData['Prediction'] > nextData['Prediction']
-        ? currentData['Prediction']
-        : nextData['Prediction'];
+    int maxPci = currentData['Label'] > nextData['Label']
+        ? currentData['Label']
+        : nextData['Label'];
 
     // Create properties for LineString
     Map<String, dynamic> properties = {
-      'Avg. Speed': 3.6*avgVelocity,
+      'Avg. Speed': 3.6 * avgVelocity,
       'PCI': maxPci,
     };
 
-    // Create coordinates for LineString
     List<List<double>> coordinates = [
       [
-        double.parse(currentData['Longitude']),
-        double.parse(currentData['Latitude']),
+        currentData['Longitude'],
+        currentData['Latitude'],
       ],
       [
-        double.parse(nextData['Longitude']),
-        double.parse(nextData['Latitude']),
+        nextData['Longitude'],
+        nextData['Latitude'],
       ],
     ];
 
@@ -62,6 +56,61 @@ String convertToFirstJsonFormat2(String fileContent) {
 
   // Convert the FeatureCollection dictionary to JSON format
   String jsonString = jsonEncode(featureCollection);
+  return jsonString;
+}
 
+String convertToGeoJsonFormat(List<PciData> pciDataList) {
+  List<Map<String, dynamic>> features = [];
+  for (int i = 0; i < pciDataList.length - 1; i++) {
+    PciData currentData = pciDataList[i];
+    PciData nextData = pciDataList[i + 1];
+    double avgVelocity = (currentData.velocity + nextData.velocity) / 2;
+    int maxPci =
+        currentData.pci > nextData.pci ? currentData.pci : nextData.pci;
+
+    // Create properties for LineString
+    Map<String, dynamic> properties = {
+      'Avg. Speed': 3.6 * avgVelocity,
+      'PCI': maxPci,
+    };
+
+    List<List<double>> coordinates = [
+      [
+        currentData.longitude,
+        currentData.latitude,
+      ],
+      [
+        nextData.longitude,
+        nextData.latitude,
+      ],
+    ];
+
+    // Create geometry for LineString
+    Map<String, dynamic> geometry = {
+      'type': 'LineString',
+      'coordinates': coordinates,
+    };
+
+    // Create feature for LineString
+    Map<String, dynamic> feature = {
+      'type': 'Feature',
+      'properties': properties,
+      'geometry': geometry,
+    };
+
+    // Add feature to features list
+    features.add(feature);
+  }
+
+  // Create the FeatureCollection dictionary
+  Map<String, dynamic> featureCollection = {
+    'type': 'FeatureCollection',
+    'features': features,
+  };
+  // Convert the FeatureCollection dictionary to JSON format
+  String jsonString = jsonEncode(featureCollection);
+  if (kDebugMode) {
+    print(jsonString);
+  }
   return jsonString;
 }
