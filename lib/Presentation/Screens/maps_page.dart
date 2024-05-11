45bad4201e3b6f04b77929e34abfaad12a8fe209
Vikash Kumar <vk4097440@gmail.com>
2024-05-11 09:42:00 +0530

    added drrp layer

--- lib/Presentation/Screens/maps_page.dart
+++ lib/Presentation/Screens/maps_page.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../../Functions/load_geo_json.dart';
@@ -7,6 +10,7 @@ import '../../Objects/data.dart';
 import '../../Objects/polyline_obj.dart';
 import '../Widget/path_feature.dart';
 import '../Widget/maptype_dropdown.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -38,100 +42,193 @@ class _MapPageState extends State<MapPage> {
     }
   }
 
+  bool isDrrpLayerVisible = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: SizedBox(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned.fill(
               child: SizedBox(
-                child: GoogleMap(
-                  mapType: googleMapType,
-                  initialCameraPosition: CameraPosition(
-                    target: LatLng(
-                        devicePosition.latitude, devicePosition.longitude),
-                    zoom: 15,
+                child: SizedBox(
+                  child: GoogleMap(
+                    mapType: googleMapType,
+                    initialCameraPosition: CameraPosition(
+                      target: LatLng(
+                          devicePosition.latitude, devicePosition.longitude),
+                      zoom: 15,
+                    ),
+                    onMapCreated: (GoogleMapController controller) {
+                      mapController = controller;
+                    },
+                    polylines: polylines,
+                    zoomControlsEnabled: false,
                   ),
-                  onMapCreated: (GoogleMapController controller) {
-                    mapController = controller;
-                  },
-                  polylines: polylines,
-                  zoomControlsEnabled: false,
                 ),
               ),
             ),
-          ),
-          Positioned(
-            top: 50,
-            right: 10,
-            child: MapTypeDropdown(
-              onChanged: (p0) => setState(() {}),
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Container(
+                color: Colors.white,
+                height: MediaQuery.of(context).size.height * 0.08,
+                width: MediaQuery.of(context).size.width,
+              ),
             ),
-          ),
-          Positioned(
-            bottom: 10,
-            right: 0,
-            left: 0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () async {
-                    try {
-                      await loadGeoJsonFromFile();
-                      setState(() {});
-                    } catch (error) {
-                      if (kDebugMode) {
-                        print('Error loading GeoJSON file: $error');
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.02,
+              right: MediaQuery.of(context).size.width * 0.05,
+              left: MediaQuery.of(context).size.width * 0.05,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: isDrrpLayerVisible
+                          ? Colors.blue.withOpacity(0.1)
+                          : Colors.black.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(2.5),
+                      child: InkWell(
+                        onTap: () {
+                          isDrrpLayerVisible = !isDrrpLayerVisible;
+                          if (isDrrpLayerVisible) {
+                            plotDRRPLayer();
+                          } else {
+                            List<Polyline> polylinesToAdd = [];
+                            for (var element in polylines) {
+                              if (element.polylineId.value
+                                  .contains("plotted_polyline")) {
+                                polylinesToAdd.add(element);
+                              }
+                            }
+                            polylines.clear();
+                            polylines.addAll(polylinesToAdd);
+                          }
+                          setState(() {});
+                        },
+                        child: Row(
+                          children: [
+                            const Gap(10),
+                            Text(
+                              "DRRP Roads",
+                              style: GoogleFonts.inter(
+                                color: isDrrpLayerVisible
+                                    ? Colors.blue
+                                    : Colors.black,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16,
+                              ),
+                            ),
+                            const Gap(10),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      const Gap(10),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(0.1),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(2.5),
+                          child: Row(
+                            children: [
+                              const Gap(10),
+                              Text(
+                                "Map Type ",
+                                style: GoogleFonts.inter(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              const Gap(5),
+                              MapTypeDropdown(
+                                onChanged: (p0) => setState(() {}),
+                              ),
+                              const Gap(10),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 10,
+              right: 0,
+              left: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () async {
+                      try {
+                        await loadGeoJsonFromFile();
+                        setState(() {});
+                      } catch (error) {
+                        if (kDebugMode) {
+                          print('Error loading GeoJSON file: $error');
+                        }
                       }
-                    }
-                  },
-                  child: Text(
-                    'Open File',
-                    style: GoogleFonts.inter(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16,
+                    },
+                    child: Text(
+                      'Open File',
+                      style: GoogleFonts.inter(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                      ),
                     ),
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    plotMap();
-                    Future.delayed(const Duration(milliseconds: 500)).then(
-                      (value) => animateToLocation(),
-                    );
-                    setState(() {});
-                  },
-                  child: Text(
-                    'Plot Map',
-                    style: GoogleFonts.inter(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16,
+                  ElevatedButton(
+                    onPressed: () async {
+                      plotMap();
+                      Future.delayed(const Duration(milliseconds: 500)).then(
+                        (value) => animateToLocation(),
+                      );
+                      setState(() {});
+                    },
+                    child: Text(
+                      'Plot Map',
+                      style: GoogleFonts.inter(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                      ),
                     ),
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    polylines.clear();
-                    polylineObj.clear();
-                    setState(() {});
-                  },
-                  child: Text(
-                    'Clear',
-                    style: GoogleFonts.inter(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16,
+                  ElevatedButton(
+                    onPressed: () async {
+                      polylines.clear();
+                      polylineObj.clear();
+                      setState(() {});
+                    },
+                    child: Text(
+                      'Clear',
+                      style: GoogleFonts.inter(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -152,7 +249,7 @@ class _MapPageState extends State<MapPage> {
 
       Polyline temporaryPolyline = Polyline(
         consumeTapEvents: true,
-        polylineId: PolylineId('polyline$i'),
+        polylineId: PolylineId('plotted_polyline$i'),
         color: getRoadColor(feature['properties']['PCI'].toString()),
         width: 10,
         endCap: Cap.roundCap,
@@ -189,12 +286,60 @@ class _MapPageState extends State<MapPage> {
       );
       polylines.add(temporaryPolyline);
     }
-    jsonData.clear;
+    jsonData = {};
     setState(() {});
   }
 
+  Future<void> plotDRRPLayer() async{
+    await readDrrpRoadFile();
+    features = jsonData['features'];
+    List<Polyline> polylinesToAdd = [];
+    for (var element in polylines) {
+      if (element.polylineId.value.contains("plotted_polyline")) {
+        polylinesToAdd.add(element);
+      }
+    }
+    polylines.clear();
+
+    for (int i = 0; i < features.length; i++) {
+      List<LatLng> points = [];
+      Map<String, dynamic> feature = features[i];
+      List<dynamic> coordinates = feature['geometry']['coordinates'];
+      for (var data in coordinates) {
+        points.add(
+          LatLng(data[1], data[0]),
+        );
+      }
+
+      Polyline temporaryPolyline = Polyline(
+        polylineId: PolylineId('drrp_polyline$i'),
+        color: getRoadColor(feature['properties']['PCI'].toString()),
+        width: 5,
+        endCap: Cap.roundCap,
+        startCap: Cap.roundCap,
+        jointType: JointType.round,
+        points: points,
+        patterns: [
+          PatternItem.dash(10),
+          PatternItem.gap(10),
+        ],
+      );
+
+      polylines.add(temporaryPolyline);
+      polylines.addAll(polylinesToAdd);
+    }
+    jsonData = {};
+    setState(() {});
+  }
+
+  Future<void> readDrrpRoadFile() async {
+    String jsonString =
+        await rootBundle.loadString("lib/Assets/Roads/road_drrp.geojson");
+    jsonData = jsonDecode(jsonString);
+  }
+
   // Function to animate the camera to the location of the polylines
-  void animateToLocation() async {
+  Future<void> animateToLocation() async {
     double minLat = features[0]['geometry']['coordinates'][0][1];
     double minLng = features[0]['geometry']['coordinates'][0][0];
     double maxLat =
