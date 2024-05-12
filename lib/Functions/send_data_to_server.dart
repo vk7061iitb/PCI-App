69d02ffc6f06d838f27eb2354ba72b4db642900f
Vikash <vk4097440@gmail.com>
2024-05-13 05:19:54 +0530

    remove http function and updated sendData()

--- lib/Functions/send_data_to_server.dart
+++ lib/Functions/send_data_to_server.dart
@@ -1,38 +1,11 @@
 import 'dart:convert';
-import 'package:flutter/foundation.dart';
 import 'package:pci_app/Objects/data.dart';
 import 'package:web_socket_channel/io.dart';
 
-import 'package:http/http.dart' as http;
-
-sendData() async {
+Future<void> sendData() async {
   List<Map<String, dynamic>> jsonList =
-      accDataList.map((accData) => accData.toJson()).toList();
+      accDataList.map((data) => data.toJson()).toList();
   String jsonString = jsonEncode(jsonList);
-
-  final channel = IOWebSocketChannel.connect('ws://10.96.6.13:5000/');
+  final channel = IOWebSocketChannel.connect('ws://192.168.173.142:5000');
   channel.sink.add(jsonString);
-}
-
-Future<void> sendDataToServer() async {
-  String url = 'http://192.168.58.247:5000';
-  List<Map<String, dynamic>> jsonList =
-      accDataList.map((accData) => accData.toJson()).toList();
-  String jsonString = jsonEncode(jsonList);
-  dynamic data = jsonString;
-  final response = await http.post(
-    Uri.parse(url),
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode(data),
-  );
-
-  if (response.statusCode == 200) {
-    if (kDebugMode) {
-      print('Data sent successfully');
-    }
-  } else {
-    throw Exception('Failed to send data');
-  }
 }
