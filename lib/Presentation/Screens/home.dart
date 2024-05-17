d53c654f5a0d84d745c3e1897ced5466b691f4df
Vikash <vk4097440@gmail.com>
2024-05-17 18:56:14 +0530

    moved all permission from initState to main.dart

--- lib/Presentation/Screens/home.dart
+++ lib/Presentation/Screens/home.dart
@@ -1,10 +1,6 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:pci_app/Functions/request_storage_permission.dart';
 import 'package:pci_app/Presentation/Screens/maps_page.dart';
 import 'package:pci_app/Presentation/Screens/sensor_page.dart';
-import '../../Functions/request_location_permission.dart';
 import '../../Objects/data.dart';
 import 'show_history.dart';
 
@@ -35,38 +31,6 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    checkPermission();
-    streamSubscriptions.add(
-      Geolocator.getPositionStream(
-          locationSettings: AndroidSettings(
-        forceLocationManager: false,
-        accuracy: LocationAccuracy.best,
-        distanceFilter: 0,
-        intervalDuration: const Duration(milliseconds: 250),
-        foregroundNotificationConfig: const ForegroundNotificationConfig(
-          notificationTitle: 'PCI App',
-          notificationText: 'Collecting Location Data',
-          notificationChannelName: 'PCI App',
-          setOngoing: true,
-          enableWakeLock: true,
-          color: Colors.blueAccent,
-        ),
-      )).listen(
-        (event) {
-          devicePosition = event;
-          if (isRecordingData) {
-            if (kDebugMode) {
-              print("${devicePosition.latitude} ${devicePosition.longitude}");
-            }
-          }
-        },
-      ),
-    );
-  }
-
-  void checkPermission() async {
-    await requestLocationPermission();
-    await requestStoragePermission();
   }
 
   @override
