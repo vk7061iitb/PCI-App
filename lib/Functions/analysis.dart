53de4482c57506e084eebaa9e14550943826796e
Vikash <vk4097440@gmail.com>
2024-06-05 15:41:04 +0530

    added new function which correctly solves the frequency issue

--- lib/Functions/analysis.dart
+++ lib/Functions/analysis.dart
@@ -1,20 +1,78 @@
 import '../Objects/data_points.dart';
 
-List<AccData> correctSamplingRate(List<AccData> accData) {
-  List<AccData> correctedAccData = [];
-  correctedAccData.add(accData[0]);
+AccData average(AccData accData1, AccData accData2) {
+  AccData avgAccData;
+  avgAccData = AccData(
+      xAcc: (accData1.xAcc + accData2.xAcc) / 2.0,
+      yAcc: (accData1.yAcc + accData2.yAcc) / 2.0,
+      zAcc: (accData1.zAcc + accData2.zAcc) / 2.0,
+      latitude: accData2.latitude,
+      longitude: accData2.longitude,
+      speed: accData2.speed,
+      accTime: accData2.accTime);
+  return avgAccData;
+}
 
-  for (int i = 1; i < accData.length; i++) {
-    double timeDifference = accData[i]
-        .accTime
-        .difference(correctedAccData.last.accTime)
-        .inMilliseconds
-        .toDouble();
+AccData linInterpolate(AccData before, AccData after, DateTime curr) {
+  double fraction = (curr.difference(before.accTime).inMicroseconds) /
+      (after.accTime.difference(before.accTime).inMicroseconds).toDouble();
+  double interpolatedX = before.xAcc + (after.xAcc - before.xAcc) * fraction;
+  double interpolatedY = before.yAcc + (after.yAcc - before.yAcc) * fraction;
+  double interpolatedZ = before.zAcc + (after.zAcc - before.zAcc) * fraction;
 
-    if (timeDifference >= 15) {
-      correctedAccData.add(accData[i]);
+  AccData interpolatedData = AccData(
+      xAcc: interpolatedX,
+      yAcc: interpolatedY,
+      zAcc: interpolatedZ,
+      latitude: before.latitude,
+      longitude: before.longitude,
+      speed: before.speed,
+      accTime: curr);
+
+  return interpolatedData;
+}
+
+List<AccData> downsampleTo50Hz(List<AccData> accDataList) {
+  List<AccData> downsampledList = [];
+  if (accDataList.isEmpty) return downsampledList;
+
+  DateTime startTime = accDataList.first.accTime;
+  DateTime currentTime = startTime.add(
+    const Duration(microseconds: 20000),
+  );
+  int interval = 20000; // in microseconds
+
+  downsampledList.add(accDataList[0]);
+
+  for (int i = 1; i < accDataList.length - 1; i++) {
+    AccData before = accDataList[0];
+    AccData after;
+
+    if (accDataList[i].accTime.isBefore(currentTime) ||
+        accDataList[i].accTime.isAtSameMomentAs(currentTime)) {
+      before = average(accDataList[i], accDataList[i - 1]);
+    }
+    if (accDataList[i + 1].accTime.isAfter(currentTime) ||
+        accDataList[i + 1].accTime.isAtSameMomentAs(currentTime)) {
+      after = accDataList[i + 1];
+
+      if (accDataList[i].accTime.isAfter(currentTime) &&
+          accDataList[i + 1].accTime.isAfter(currentTime)) {
+        AccData genData =
+            linInterpolate(downsampledList.last, after, currentTime);
+        downsampledList.add(
+          genData,
+        );
+      } else {
+        // Finding the interpolated data
+        AccData interPolatedAccData =
+            linInterpolate(before, after, currentTime);
+        downsampledList.add(interPolatedAccData);
+      }
+
+      currentTime = currentTime.add(Duration(microseconds: interval));
     }
   }
 
-  return correctedAccData;
+  return downsampledList;
 }
