6db85a7501f70650eca88f8476168736224fd613
Vikash Kumar <vk4097440@gmail.com>
2024-07-03 01:20:48 +0530

    drpdown to select vehicle type

---
+++ lib/src/Presentation/Screens/SensorPage/widgets/vehicle_type_dropdown.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pci_app/Objects/data.dart';
+import 'package:pci_app/Utils/get_icon.dart';
+import 'package:pci_app/src/Presentation/Controllers/response_controller.dart';
+
+class VehicleType extends StatelessWidget {
+  const VehicleType({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    ResponseController responseController = Get.find();
+    return Center(
+      child: DropdownMenu<String>(
+        width: MediaQuery.of(context).size.width * 0.9,
+        leadingIcon: Obx(() {
+          return Icon(
+            getIcon(responseController.dropdownValue),
+            color: Colors.black,
+          );
+        }),
+        initialSelection: vehicleType.first,
+        onSelected: (String? value) {
+          responseController.dropdownValue = value!;
+        },
+        dropdownMenuEntries:
+            vehicleType.map<DropdownMenuEntry<String>>((String value) {
+          return DropdownMenuEntry<String>(value: value, label: value);
+        }).toList(),
+        textStyle: GoogleFonts.inter(
+          color: Colors.black,
+          fontWeight: FontWeight.w400,
+          fontSize: 18,
+        ),
+        menuHeight: 200,
+        menuStyle: MenuStyle(
+          elevation: const WidgetStatePropertyAll(1),
+          shape: WidgetStatePropertyAll(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          isDense: true,
+          constraints: BoxConstraints(
+            maxHeight: 50,
+            minWidth: MediaQuery.of(context).size.width * 0.8,
+          ),
+          labelStyle: GoogleFonts.inter(
+            color: Colors.black,
+            fontWeight: FontWeight.w400,
+            fontSize: 16,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            gapPadding: 1,
+            borderSide: const BorderSide(
+              color: Colors.black,
+              width: 1,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
