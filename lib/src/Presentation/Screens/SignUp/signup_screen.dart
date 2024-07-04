8396ea988f1f82d5ffa2a667b37b167d162032ae
Vikash Kumar <vk4097440@gmail.com>
2024-07-04 11:22:12 +0530

    completed getX intigration

--- lib/src/Presentation/Screens/SignUp/signup_screen.dart
+++ lib/src/Presentation/Screens/SignUp/signup_screen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:pci_app/src/Presentation/Controllers/signup_controller.dart';
+import 'package:pci_app/src/Presentation/Widgets/snackbar.dart';
+import '../../../../Objects/data.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    SignupController signupController = Get.put(SignupController());
     return Scaffold(
       backgroundColor: const Color(0xFFF3EDF5),
       appBar: AppBar(
@@ -61,141 +66,165 @@ class SignupScreen extends StatelessWidget {
                 const Gap(10),
                 // Email Field
                 Form(
-                  child: TextFormField(
-                    autocorrect: true,
-                    keyboardType: TextInputType.name,
-                    decoration: const InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.black,
-                          width: 2,
+                  key: signupController.signupFormKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: signupController.nameController,
+                        autocorrect: true,
+                        keyboardType: TextInputType.name,
+                        decoration: const InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black,
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            ),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            ),
+                          ),
+                          isDense: false,
+                          contentPadding: EdgeInsets.all(15),
+                          prefixIcon: Icon(Icons.mail_outline),
+                          hintText: "Enter Your Name",
                         ),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Please enter a name";
+                          }
+                          return null;
+                        },
+                      ),
+                      const Gap(20),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Email",
+                          style: GoogleFonts.inter(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
+                          ),
                         ),
                       ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15),
+                      const Gap(10),
+                      // Email Field
+                      TextFormField(
+                        controller: signupController.emailController,
+                        autocorrect: true,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black,
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            ),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            ),
+                          ),
+                          isDense: false,
+                          contentPadding: EdgeInsets.all(15),
+                          prefixIcon: Icon(Icons.mail_outline),
+                          hintText: "Enter Your Email Address",
+                        ),
+                        validator: (value) {
+                          if (value!.length < 6 || !value.contains("@")) {
+                            return "Please enter a valid email address";
+                          }
+                          return null;
+                        },
+                      ),
+                      const Gap(20),
+                      // Phone Number Field
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Phone",
+                          style: GoogleFonts.inter(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
+                          ),
                         ),
                       ),
-                      isDense: false,
-                      contentPadding: EdgeInsets.all(15),
-                      prefixIcon: Icon(Icons.mail_outline),
-                      hintText: "Enter Your Name",
-                    ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Please enter a name";
-                      }
-                      return null;
-                    },
+                      const Gap(10),
+                      TextFormField(
+                        controller: signupController.phoneController,
+                        autocorrect: true,
+                        keyboardType: TextInputType.phone,
+                        decoration: const InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black,
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            ),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            ),
+                          ),
+                          isDense: false,
+                          contentPadding: EdgeInsets.all(15),
+                          prefixIcon: Icon(Icons.phone),
+                          hintText: "Enter Your Phone Number",
+                        ),
+                        validator: (value) {
+                          if (value!.length != 10) {
+                            return "Please enter a valid phone number";
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
                   ),
                 ),
-                const Gap(20),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Email",
-                    style: GoogleFonts.inter(
-                      fontSize: 18,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ),
-                const Gap(10),
-                // Email Field
-                Form(
-                  child: TextFormField(
-                    autocorrect: true,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.black,
-                          width: 2,
-                        ),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15),
-                        ),
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15),
-                        ),
-                      ),
-                      isDense: false,
-                      contentPadding: EdgeInsets.all(15),
-                      prefixIcon: Icon(Icons.mail_outline),
-                      hintText: "Enter Your Email Address",
-                    ),
-                    validator: (value) {
-                      if (value!.length < 6 || !value.contains("@")) {
-                        return "Please enter a valid email address";
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                const Gap(20),
-                // Phone Number Field
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Phone",
-                    style: GoogleFonts.inter(
-                      fontSize: 18,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ),
-                const Gap(10),
-                Form(
-                  child: TextFormField(
-                    autocorrect: true,
-                    keyboardType: TextInputType.phone,
-                    decoration: const InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.black,
-                          width: 2,
-                        ),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15),
-                        ),
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15),
-                        ),
-                      ),
-                      isDense: false,
-                      contentPadding: EdgeInsets.all(15),
-                      prefixIcon: Icon(Icons.phone),
-                      hintText: "Enter Your Phone Number",
-                    ),
-                    validator: (value) {
-                      if (value!.length != 10) {
-                        return "Please enter a valid phone number";
-                      }
-                      return null;
-                    },
-                  ),
-                ),
+
                 const Gap(20),
                 Row(
                   children: [
                     Expanded(
                       child: ElevatedButton(
-                        onPressed: () {},
+                        onPressed: () async {
+                          // Sign up logic
+                          if (signupController.signupFormKey.currentState!
+                              .validate()) {
+                            await signupController.signUp().then((_) {
+                              if (signupController.isSignedUp) {
+                                Get.offNamed(myRoutes.loginRoute);
+                                Get.showSnackbar(
+                                  customGetSnackBar(
+                                      "Success! Account created successfully."),
+                                );
+                              } else {
+                                Get.showSnackbar(
+                                  customGetSnackBar(
+                                      "Error! User already exists"),
+                                );
+                              }
+                            });
+                          }
+                        },
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.black87,
                           padding: const EdgeInsets.all(8),
@@ -228,7 +257,9 @@ class SignupScreen extends StatelessWidget {
                         ),
                       ),
                       TextButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          Get.back();
+                        },
                         child: Text(
                           "Sign in",
                           style: GoogleFonts.inter(
