9c7c025d9510b679e9adfbf399e9c132bd393de6
Vikash Kumar <vk4097440@gmail.com>
2024-07-07 05:24:05 +0530

    switched from dropdown to popup menu button

--- lib/src/Presentation/Screens/MapsPage/widget/maptype_dropdown.dart
+++ lib/src/Presentation/Screens/MapsPage/widget/maptype_dropdown.dart
@@ -1,3 +1,7 @@
+/* 
+This widget is used to display the map type dropdown. It is used to select the type of map to be displayed on the Map Screen.
+*/
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -16,12 +20,26 @@ class _MapTypeDropdownState extends State<MapTypeDropdown> {
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      isDense: true,
-      isExpanded: false,
-      value: dropdownValue,
-      onChanged: (String? value) {
-        // This is called when the user selects an item.
+    return PopupMenuButton<String>(
+      style: ButtonStyle(
+        backgroundColor: WidgetStateProperty.all<Color>(Colors.white),
+        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50.0),
+            side: const BorderSide(
+              color: Colors.black38,
+              width: 1,
+            ),
+          ),
+        ),
+      ),
+      tooltip: 'Select Map Type',
+      padding: const EdgeInsets.all(14),
+      icon: const Icon(
+        Icons.map,
+        color: Colors.black,
+      ),
+      onSelected: (String value) {
         setState(() {
           switch (value) {
             case 'Normal':
@@ -30,50 +48,54 @@ class _MapTypeDropdownState extends State<MapTypeDropdown> {
             case 'Satellite':
               googleMapType = googlemapType[1];
               break;
-    
             case 'Hybrid':
               googleMapType = googlemapType[2];
               break;
-    
-            case 'Teraain':
+            case 'Terrain':
               googleMapType = googlemapType[3];
               break;
-    
             case 'None':
               googleMapType = googlemapType[4];
               break;
-    
             default:
               googleMapType = googlemapType[1];
           }
-          dropdownValue = value!;
+          dropdownValue = value;
           if (kDebugMode) {
             print('Map Type: $dropdownValue');
           }
           widget.onChanged(dropdownValue);
         });
       },
-      style: GoogleFonts.inter(
-        color: Colors.black,
-        fontWeight: FontWeight.w500,
-        fontSize: 16,
-      ),
-      elevation: 1,
-      underline: const SizedBox(),
-      borderRadius: BorderRadius.circular(15),
-      items: mapType.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(
-            value,
-            style: GoogleFonts.inter(
-              color: Colors.blue,
-              fontWeight: FontWeight.w500,
-              fontSize: 16,
+      itemBuilder: (BuildContext context) {
+        return <PopupMenuEntry<String>>[
+          PopupMenuItem<String>(
+            enabled: false,
+            child: Text(
+              'Map Type',
+              style: GoogleFonts.inter(
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+                fontSize: 16,
+              ),
             ),
           ),
-        );
-      }).toList(),
+          const PopupMenuDivider(),
+          ...mapType.map<PopupMenuItem<String>>((String value) {
+            return PopupMenuItem<String>(
+              value: value,
+              child: Text(
+                value,
+                style: GoogleFonts.inter(
+                  color: value == dropdownValue ? Colors.blue : Colors.black,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                ),
+              ),
+            );
+          }),
+        ];
+      },
     );
   }
 }
