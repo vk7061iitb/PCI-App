21c35bfd0b48fb69bdade7e71133790c33c25928
Vikash Kumar <vk4097440@gmail.com>
2024-08-08 17:34:35 +0530

    Improved the handling of the location permission

--- lib/src/Presentation/Controllers/location_permission.dart
+++ lib/src/Presentation/Controllers/location_permission.dart
@@ -7,6 +7,12 @@ class LocationController extends GetxController {
   final RxBool _serviceEnabled = false.obs;
   final Rx<LocationPermission> _permission = LocationPermission.denied.obs;
 
+  final _textStyle = GoogleFonts.inter(
+    color: Colors.blue,
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+  );
+
   // Getters
   bool get serviceEnabled => _serviceEnabled.value;
   LocationPermission get permission => _permission.value;
@@ -24,162 +30,119 @@ class LocationController extends GetxController {
   // Check GPS service and permissions
   void checkAndRequestLocation() async {
     await checkLocationService();
+    await checkLocationPermission();
 
-    if (serviceEnabled) {
-      await checkLocationPermission();
-      if (permission != LocationPermission.always) {
-        await requestAllTimeLocationPermission().then((_) {
-          checkAndRequestLocation();
-        });
-      }
+    if (!serviceEnabled || permission != LocationPermission.always) {
+      showLocationDialog();
     }
   }
 
   // Check if GPS service is enabled
   Future<void> checkLocationService() async {
     _serviceEnabled.value = await Geolocator.isLocationServiceEnabled();
-    if (!_serviceEnabled.value) {
-      Get.dialog(
-        AlertDialog(
-          title: Text(
-            'Location Services Required',
-            style: GoogleFonts.inter(
-              color: Colors.black,
-              fontWeight: FontWeight.w700,
-              fontSize: 24,
-            ),
-          ),
-          content: Text(
-            'To use this app, you need to enable location services on your device.',
-            style: GoogleFonts.inter(
-              color: Colors.black54,
-              fontWeight: FontWeight.w500,
-              fontSize: 16,
-            ),
-          ),
-          actions: <Widget>[
-            OutlinedButton(
-              onPressed: () {
-                Geolocator.openLocationSettings();
-                Get.back();
-              },
-              child: Text(
-                'Enable Location',
-                style: GoogleFonts.inter(
-                  color: Colors.blue,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
-    }
   }
 
   // Check location permission status
   Future<void> checkLocationPermission() async {
     _permission.value = await Geolocator.checkPermission();
-    if (_permission.value == LocationPermission.denied ||
-        _permission.value == LocationPermission.deniedForever) {
-      Get.dialog(
-        AlertDialog(
-          title: Text(
-            'Location Services Required',
-            style: GoogleFonts.inter(
-              color: Colors.black,
-              fontWeight: FontWeight.w700,
-              fontSize: 24,
-            ),
-          ),
-          content: Text(
-            'Please grant location access to this app to continue.',
-            style: GoogleFonts.inter(
-              color: Colors.black54,
-              fontWeight: FontWeight.w500,
-              fontSize: 16,
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () async {
-                await Geolocator.requestPermission();
-                Get.back();
-                await checkLocationPermission(); // Re-check the permission after requesting
-              },
-              child: Text(
-                'Grant Permission',
-                style: GoogleFonts.inter(
-                  color: Colors.blue,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
-    }
   }
 
-// Request "All the Time" location permission
-  Future<void> requestAllTimeLocationPermission() async {
-    if (_permission.value == LocationPermission.always) {
-      debugPrint('Location permission is already set to "All the Time".');
-      return;
+  // Show a single dialog based on the current status
+  void showLocationDialog() {
+    String title;
+    String content;
+
+    if (!serviceEnabled) {
+      title = 'Enable Location Services';
+      content =
+          'To use this app, you need to enable location services on your device.';
+    } else if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+      title = 'Grant Location Permission';
+      content = 'Please grant location access to this app to continue.';
     } else {
-      Get.dialog(
-        AlertDialog(
-          title: Text(
-            'Please Grant All Time Location Access',
-            style: GoogleFonts.inter(
-              color: Colors.black,
-              fontWeight: FontWeight.w700,
-              fontSize: 24,
-            ),
+      title = 'Grant All Time Location Access';
+      content =
+          'Please grant "Allow all the Time" location access for better app performance.';
+    }
+
+    Get.dialog(
+      AlertDialog(
+        title: Text(
+          title,
+          style: GoogleFonts.inter(
+            color: Colors.black,
+            fontWeight: FontWeight.w700,
+            fontSize: 24,
           ),
-          content: Text(
-            'Please grant "Allow all the Time" location access.',
-            style: GoogleFonts.inter(
-              color: Colors.black54,
-              fontWeight: FontWeight.w500,
-              fontSize: 16,
-            ),
+        ),
+        content: Text(
+          content,
+          style: GoogleFonts.inter(
+            color: Colors.black54,
+            fontWeight: FontWeight.w500,
+            fontSize: 16,
           ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () async {
-                await Geolocator.requestPermission();
-                Get.back();
-                await checkLocationPermission(); // Re-check the permission after requesting
-              },
-              child: Text(
-                'Grant All Time Access',
-                style: GoogleFonts.inter(
-                  color: Colors.blue,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                Geolocator.openAppSettings();
-                Get.back();
-              },
-              child: Text(
-                'Go to Settings',
-                style: GoogleFonts.inter(
-                  color: Colors.blue,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ],
+        ),
+        actions: _buildDialogActions(),
+      ),
+    );
+  }
+
+  // Build dialog actions based on the current status
+  List<Widget> _buildDialogActions() {
+    List<Widget> actions = [];
+
+    if (!serviceEnabled) {
+      actions.add(
+        TextButton(
+          onPressed: () {
+            Geolocator.openLocationSettings();
+            Get.back();
+          },
+          child: Text('Enable Location', style: _textStyle),
         ),
       );
     }
+
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+      actions.add(
+        TextButton(
+          onPressed: () async {
+            await Geolocator.requestPermission();
+            Get.back();
+            checkAndRequestLocation(); // Re-check the status after requesting
+          },
+          child: Text('Grant Permission', style: _textStyle),
+        ),
+      );
+    }
+
+    if (permission != LocationPermission.always) {
+      actions.add(
+        TextButton(
+          onPressed: () async {
+            await Geolocator.requestPermission();
+            Get.back();
+            checkAndRequestLocation(); // Re-check the status after requesting
+          },
+          child: Text('Grant All Time Access', style: _textStyle),
+        ),
+      );
+      actions.add(
+        TextButton(
+          onPressed: () {
+            Geolocator.openAppSettings();
+            Get.back();
+          },
+          child: Text(
+            'Go to Settings',
+            style: _textStyle,
+          ),
+        ),
+      );
+    }
+    return actions;
   }
 }
