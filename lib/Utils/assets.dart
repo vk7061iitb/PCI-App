4266eea05ebe470a141cf963b4a1ba77e2ad7de7
Vikash Kumar <vk4097440@gmail.com>
2024-08-10 10:36:54 +0530

    added comments

--- lib/Utils/assets.dart
+++ lib/Utils/assets.dart
@@ -1,4 +1,7 @@
-// This file contains the path of the assets used in the application.
+// This file contains the path of the assets and DRRP Road Layer used in the application.
+
+// AssetsPath class is created to store the path of the assets used in the application.
+// It contains the path of the images, icons, and roads used in the application.
 
 class AssetsPath {
   // Images
