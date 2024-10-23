000bcbe33fd59b7fdf97cfc8013d20daf3abc7c6
Vikash Kumar <vk4097440@gmail.com>
2024-10-23 17:46:31 +0530

    Refactor customGetSnackBar function to include an icon parameter

--- lib/src/Presentation/Widgets/snackbar.dart
+++ lib/src/Presentation/Widgets/snackbar.dart
@@ -2,46 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-SnackBar customSnackBar(String message) {
-  return SnackBar(
-    content: Text(
-      message,
-      style: GoogleFonts.inter(
-        color: Colors.black87,
-        fontWeight: FontWeight.normal,
-        fontSize: 14,
-      ),
-    ),
-    showCloseIcon: true,
-    behavior: SnackBarBehavior.floating,
-    closeIconColor: Colors.black87,
-    backgroundColor: const Color(0xFFF3EDF5),
-    padding: const EdgeInsets.all(12),
-    duration: const Duration(seconds: 5),
-    elevation: 2.0,
-    shape: const RoundedRectangleBorder(
-      side: BorderSide.none,
-      borderRadius: BorderRadius.all(
-        Radius.circular(20),
-      ),
-    ),
-  );
-}
-
-GetSnackBar customGetSnackBar(String message) {
+GetSnackBar customGetSnackBar(String message, IconData icon) {
   return GetSnackBar(
+    icon: Icon(
+      icon,
+      color: Colors.white,
+    ),
     messageText: Text(
       message,
       style: GoogleFonts.inter(
-        color: Colors.black87,
+        color: Colors.white,
         fontWeight: FontWeight.normal,
-        fontSize: 14,
+        fontSize: 16,
       ),
     ),
     isDismissible: true,
     snackPosition: SnackPosition.BOTTOM,
-    backgroundColor: const Color(0xFFF3EDF5),
-    margin: const EdgeInsets.all(12),
+    backgroundColor: Colors.black87,
+    margin: const EdgeInsets.all(15),
     duration: const Duration(seconds: 5),
     borderRadius: 20,
     boxShadows: [
