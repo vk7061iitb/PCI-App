8f2d27b98059f04109564b6e81bb4f4819185d04
Vikash Kumar <vk4097440@gmail.com>
2024-10-23 17:49:31 +0530

    Refactor UserData class to include userRole property

--- lib/src/Models/user_data.dart
+++ lib/src/Models/user_data.dart
@@ -2,12 +2,14 @@
 // This model class is used to store the user data in the database
 
 class UserData {
+  final String userRole;
   final String? userID;
   final String phoneNumber;
   final String email;
 
   UserData({
     this.userID,
+    required this.userRole,
     required this.phoneNumber,
     required this.email,
   });
