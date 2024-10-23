14937da07d80080edf50845ac562bc72bac788ec
Vikash Kumar <vk4097440@gmail.com>
2024-10-23 17:57:08 +0530

    included map controller

--- lib/src/Presentation/Screens/MapsPage/widget/maptype_dropdown.dart
+++ lib/src/Presentation/Screens/MapsPage/widget/maptype_dropdown.dart
@@ -1,101 +1,94 @@
-/* 
-This widget is used to display the map type dropdown. It is used to select the type of map to be displayed on the Map Screen.
-*/
+// This widget is used to display the map type dropdown. It is used to select the type of map to be displayed on the Map Screen.
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:pci_app/src/Presentation/Controllers/map_page_controller.dart';
 import '../../../../../Objects/data.dart';
 
-class MapTypeDropdown extends StatefulWidget {
-  final Function(String) onChanged;
-  const MapTypeDropdown({required this.onChanged, super.key});
-
-  @override
-  State<MapTypeDropdown> createState() => _MapTypeDropdownState();
-}
-
-class _MapTypeDropdownState extends State<MapTypeDropdown> {
-  String dropdownValue = mapType[0]; // Set initial value
+class SelectMapType extends StatelessWidget {
+  const SelectMapType({super.key});
 
   @override
   Widget build(BuildContext context) {
+    MapPageController mapPageController = Get.find();
+
     return PopupMenuButton<String>(
-      style: ButtonStyle(
-        backgroundColor: WidgetStateProperty.all<Color>(Colors.white),
-        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(50.0),
-            side: const BorderSide(
-              color: Colors.black38,
-              width: 1,
+        style: ButtonStyle(
+          backgroundColor: WidgetStateProperty.all<Color>(Colors.white),
+          shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50.0),
+              side: const BorderSide(
+                color: Colors.black38,
+                width: 1,
+              ),
             ),
           ),
         ),
-      ),
-      tooltip: 'Select Map Type',
-      padding: const EdgeInsets.all(14),
-      icon: const Icon(
-        Icons.map,
-        color: Colors.black,
-      ),
-      onSelected: (String value) {
-        setState(() {
+        tooltip: 'Select Map Type',
+        padding: const EdgeInsets.all(14),
+        icon: const Icon(
+          Icons.map,
+          color: Colors.black,
+        ),
+        onSelected: (String value) {
           switch (value) {
             case 'Normal':
-              googleMapType = googlemapType[0];
+              mapPageController.backgroundMapType = googlemapType[0];
               break;
             case 'Satellite':
-              googleMapType = googlemapType[1];
+              mapPageController.backgroundMapType = googlemapType[1];
               break;
             case 'Hybrid':
-              googleMapType = googlemapType[2];
+              mapPageController.backgroundMapType = googlemapType[2];
               break;
             case 'Terrain':
-              googleMapType = googlemapType[3];
+              mapPageController.backgroundMapType = googlemapType[3];
               break;
             case 'None':
-              googleMapType = googlemapType[4];
+              mapPageController.backgroundMapType = googlemapType[4];
               break;
             default:
-              googleMapType = googlemapType[1];
+              mapPageController.backgroundMapType = googlemapType[1];
           }
-          dropdownValue = value;
+          mapPageController.dropdownvalue = value;
           if (kDebugMode) {
-            print('Map Type: $dropdownValue');
+            print('Map Type: ${mapPageController.dropdownvalue}');
           }
-          widget.onChanged(dropdownValue);
-        });
-      },
-      itemBuilder: (BuildContext context) {
-        return <PopupMenuEntry<String>>[
-          PopupMenuItem<String>(
-            enabled: false,
-            child: Text(
-              'Map Type',
-              style: GoogleFonts.inter(
-                color: Colors.black,
-                fontWeight: FontWeight.w500,
-                fontSize: 16,
-              ),
-            ),
-          ),
-          const PopupMenuDivider(),
-          ...mapType.map<PopupMenuItem<String>>((String value) {
-            return PopupMenuItem<String>(
-              value: value,
+        },
+        itemBuilder: (BuildContext context) {
+          return <PopupMenuEntry<String>>[
+            PopupMenuItem<String>(
+              enabled: false,
               child: Text(
-                value,
+                'Map Type',
                 style: GoogleFonts.inter(
-                  color: value == dropdownValue ? Colors.blue : Colors.black,
-                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
                   fontSize: 16,
                 ),
               ),
-            );
-          }),
-        ];
-      },
-    );
+            ),
+            const PopupMenuDivider(),
+            ...mapPageController.mapType
+                .map<PopupMenuItem<String>>((String value) {
+              return PopupMenuItem<String>(
+                value: value,
+                child: Text(
+                  value,
+                  style: GoogleFonts.inter(
+                    color: value == mapPageController.dropdownvalue
+                        ? Colors.blue
+                        : Colors.black,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                  ),
+                ),
+              );
+            }),
+          ];
+        });
   }
 }
