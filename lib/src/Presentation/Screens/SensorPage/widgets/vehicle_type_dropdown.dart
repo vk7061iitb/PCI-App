2e18382b31d91dcf7564b15a5cb83d8d0e99bbf2
Vikash Kumar <vk4097440@gmail.com>
2024-10-23 18:00:57 +0530

    updated the ui

--- lib/src/Presentation/Screens/SensorPage/widgets/vehicle_type_dropdown.dart
+++ lib/src/Presentation/Screens/SensorPage/widgets/vehicle_type_dropdown.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:pci_app/Objects/data.dart';
 import 'package:pci_app/Utils/get_icon.dart';
 import 'package:pci_app/src/Presentation/Controllers/response_controller.dart';
 
@@ -12,57 +11,62 @@ class VehicleType extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ResponseController responseController = Get.find();
-    return Center(
-      child: DropdownMenu<String>(
-        width: width,
-        leadingIcon: Obx(() {
-          return Icon(
-            getIcon(responseController.dropdownValue),
-            color: Colors.black,
-          );
-        }),
-        initialSelection: vehicleType.first,
-        onSelected: (String? value) {
-          responseController.dropdownValue = value!;
-        },
-        dropdownMenuEntries:
-            vehicleType.map<DropdownMenuEntry<String>>((String value) {
-          return DropdownMenuEntry<String>(value: value, label: value);
-        }).toList(),
-        textStyle: GoogleFonts.inter(
+    return Container(
+      padding: const EdgeInsets.only(
+          left: 15.0, right: 15.0, top: 12.0, bottom: 12.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(
           color: Colors.black,
-          fontWeight: FontWeight.w400,
-          fontSize: 18,
+          width: 1,
         ),
-        menuHeight: 200,
-        menuStyle: MenuStyle(
-          elevation: const WidgetStatePropertyAll(1),
-          shape: WidgetStatePropertyAll(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15),
-            ),
-          ),
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          isDense: true,
-          constraints: BoxConstraints(
-            maxHeight: 50,
-            minWidth: MediaQuery.of(context).size.width * 0.8,
-          ),
-          labelStyle: GoogleFonts.inter(
-            color: Colors.black,
-            fontWeight: FontWeight.w400,
-            fontSize: 16,
-          ),
-          border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Obx(
+        () {
+          return DropdownButton<String>(
             borderRadius: BorderRadius.circular(15),
-            gapPadding: 1,
-            borderSide: const BorderSide(
+            isDense: true,
+            isExpanded: true,
+            icon: const Icon(
+              Icons.keyboard_arrow_down_outlined,
               color: Colors.black,
-              width: 1,
             ),
-          ),
-        ),
+            dropdownColor: Colors.white,
+            style: const TextStyle(
+              color: Colors.blue,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+            underline: Container(
+              height: 0, // Removes default underline
+            ),
+            value: responseController.dropdownValue,
+            items: ["Car", "Bike", "Auto", "Bus", "Walk"]
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Row(
+                  children: [
+                    Icon(getIcon(value)),
+                    const Gap(10),
+                    Text(
+                      value,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+            onChanged: (String? newValue) {
+              responseController.dropdownValue = newValue!;
+            },
+          );
+        },
       ),
     );
   }
