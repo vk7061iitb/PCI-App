5ef929df3934aa7c7386457a841fad735efebf8c
Vikash <vk5418232@gmail.com>
2024-11-24 16:10:56 +0530

    deleted unused files

--- lib/src/Presentation/Controllers/db_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-
-class DataBaseController extends GetxController {
-  final Rx<String> _dbMessage = ''.obs;
-
-  // Getters
-  String get dbMessage => _dbMessage.value;
-}
