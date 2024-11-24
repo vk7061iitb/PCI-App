db15be2b4b54d1038db99bb8d833ae1a9d605944
Vikash <vk5418232@gmail.com>
2024-11-24 16:12:47 +0530

    added getroadstats function and added pci(vel_pred) in polyline

--- lib/src/Presentation/Controllers/map_page_controller.dart
+++ lib/src/Presentation/Controllers/map_page_controller.dart
@@ -12,8 +12,7 @@ import '../../Models/stats_data.dart';
 
 class MapPageController extends GetxController {
   final RxBool _isDrrpLayerVisible = false.obs;
-  Set<Polyline> polylines = <Polyline>{};
-  final Set<Polyline> _pciPolylines = <Polyline>{};
+  final RxSet<Polyline> _pciPolylines = <Polyline>{}.obs;
   List<Road> roads = <Road>[];
   GoogleMapController? _googleMapController;
   List<Map<String, dynamic>> roadOutputDataQuery = <Map<String, dynamic>>[];
@@ -32,42 +31,101 @@ class MapPageController extends GetxController {
   ].obs;
   RxString dropdownValue = 'Normal'.obs;
   final RxBool _showCircularProgress = false.obs;
+  final RxBool _showPCIlabel = false.obs;
+  final RxBool _showIndicator = false.obs;
+
+  // variables to store the currently open road's metadata
+  Map<String, dynamic> currentRoad = <String, dynamic>{};
 
   // Getters
   bool get isDrrpLayerVisible => _isDrrpLayerVisible.value;
-  RxSet<Polyline> get getPolylines => polylines.obs;
-  RxSet<Polyline> get pciPolylines => _pciPolylines.obs;
+  RxSet<Polyline> get pciPolylines => _pciPolylines.toSet().obs;
   LatLng get getMinLat => _minLat;
   LatLng get getMaxLat => _maxLat;
   List<Road> get getRoads => roads;
   List<RoadStats> get getRoadStats => roadStats;
   GoogleMapController? get getGoogleMapController => _googleMapController;
-  List<Map<String, dynamic>> get getRoadOutputDataQuery => roadOutputDataQuery;
   List<MapType> get googlemapType => _googlemapType.toList();
   List<String> get getMapType => mapType;
   MapType get backgroundMapType => _backgroundMapType.value;
   String get dropdownvalue => dropdownValue.value;
   bool get showProgress => _showCircularProgress.value;
+  bool get showPCIlabel => _showPCIlabel.value;
+  bool get showIndicator => _showIndicator.value;
 
   // Setters
   set isDrrpLayerVisible(bool value) => _isDrrpLayerVisible.value = value;
-  set setPolylines(Set<Polyline> value) => polylines.addAll(value);
   set pciPolylines(Set<Polyline> value) => _pciPolylines.addAll(value);
   set setRoads(List<Road> value) => roads.addAll(value);
   set setGoogleMapController(GoogleMapController? value) =>
       _googleMapController = value;
-  set setRoadOutputDataQuery(List<Map<String, dynamic>> value) =>
-      roadOutputDataQuery = value;
   set setMapType(List<String> value) => mapType.addAll(value);
   set googlemapType(List<MapType> value) => _googlemapType.addAll(value);
   set backgroundMapType(MapType value) => _backgroundMapType.value = value;
   set dropdownvalue(String value) => dropdownValue.value = value;
   set showProgress(bool value) => _showCircularProgress.value = value;
+  set showPCIlabel(bool value) => _showPCIlabel.value = value;
+  set showIndicator(bool value) => _showIndicator.value = value;
+
+  void clearPolylines() {
+    _pciPolylines.clear();
+  }
+
+  void setRoadStatistics(List<Map<String, dynamic>> query) {
+    if (query.isEmpty) {
+      return;
+    }
+    if (roadStats.isNotEmpty) {
+      roadStats.clear();
+    }
+    for (var road in query) {
+      String roadName = road["roadName"];
+      dynamic roadStatistics = jsonDecode(road["stats"]);
+      // Adding the stats
+      List<RoadStatsData> roadStatsList = [];
+      for (int i = 1; i <= 5; i++) {
+        // Key represents each PCI value in the stats, which are only 1-5
+        String key = "$i";
+        roadStatsList.add(
+          RoadStatsData(
+            pci: key,
+            avgVelocity: roadStatistics[key]['avg_velocity'].toString(),
+            distanceTravelled:
+                roadStatistics[key]['distance_travelled'].toString(),
+            numberOfSegments:
+                roadStatistics[key]['number_of_segments'].toString(),
+          ),
+        );
+      }
+
+      roadStats.add(
+        RoadStats(
+          roadName: roadName,
+          roadStatsData: roadStatsList,
+        ),
+      );
+    }
+  }
+
+  double velocityToPCI(double velocity) {
+    if (velocity >= 30) {
+      return 5;
+    } else if (velocity > 20) {
+      return 4;
+    } else if (velocity > 10) {
+      return 3;
+    } else if (velocity > 5) {
+      return 2;
+    } else {
+      return 1;
+    }
+  }
 
   // fuction to plot the road data on the map
-  void plotRoadData() {
+  Future<void> plotRoadData() async {
     debugPrint("plotting road data...");
     roadStats.clear();
+    _pciPolylines.clear();
     for (var road in roadOutputDataQuery) {
       String roadName = road["roadName"];
       List<dynamic> labels = jsonDecode(road["labels"]);
@@ -106,14 +164,19 @@ class MapPageController extends GetxController {
         _maxLat = LatLng(labels[i]['latitude'], labels[i]['longitude']);
         double avgVelocity =
             (labels[i]['velocity'] + labels[i - 1]['velocity']) / 2;
+        // convert m/s to kmph
+        avgVelocity *= 3.6;
         double prediction = max(
             double.parse(labels[i]['prediction'].toString()),
             double.parse(labels[i - 1]['prediction'].toString()));
+        double velocityPCI = velocityToPCI(avgVelocity);
         // Add the polyline
         Polyline tempPolylne = Polyline(
           consumeTapEvents: true,
           polylineId: PolylineId("$roadName$i"),
-          color: getRoadColor(prediction),
+          color: _showPCIlabel.value
+              ? getRoadColor(prediction)
+              : getVelocityColor(avgVelocity),
           width: 5,
           endCap: Cap.roundCap,
           startCap: Cap.roundCap,
@@ -123,7 +186,7 @@ class MapPageController extends GetxController {
             LatLng(labels[i]['latitude'], labels[i]['longitude']),
           ],
           onTap: () {
-            debugPrint('Polyline $i tapped!');
+            debugPrint('${currentRoad['time']}');
             Get.bottomSheet(
               clipBehavior: Clip.antiAlias,
               backgroundColor: Colors.white,
@@ -154,13 +217,45 @@ class MapPageController extends GetxController {
                             fontWeight: FontWeight.bold,
                           ),
                         ),
+                        const Spacer(),
                       ],
                     ),
                     const Gap(10),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text(
-                          "PCI",
+                          '${currentRoad['filename']}',
+                          style: GoogleFonts.inter(
+                            color: Colors.black54,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.calendar_month_outlined,
+                              color: Colors.black54,
+                            ),
+                            const Gap(5),
+                            Text(
+                              '${currentRoad['time']}',
+                              style: GoogleFonts.inter(
+                                color: Colors.black54,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    const Divider(),
+                    Row(
+                      children: [
+                        Text(
+                          "PCI(prediction)",
                           style: GoogleFonts.inter(
                             color: Colors.black,
                             fontSize: 18,
@@ -181,6 +276,40 @@ class MapPageController extends GetxController {
                           child: LinearProgressIndicator(
                             minHeight: 10,
                             value: prediction / 5,
+                            backgroundColor: Colors.grey[300],
+                            valueColor: const AlwaysStoppedAnimation<Color>(
+                              Colors.black87,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Gap(10),
+                    Row(
+                      children: [
+                        Text(
+                          "PCI(velocity)",
+                          style: GoogleFonts.inter(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        const Spacer(),
+                        Text(
+                          velocityPCI.toString(),
+                          style: GoogleFonts.inter(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        const Gap(5),
+                        Expanded(
+                          child: LinearProgressIndicator(
+                            minHeight: 10,
+                            value: velocityPCI / 5,
                             backgroundColor: Colors.grey[300],
                             valueColor: const AlwaysStoppedAnimation<Color>(
                               Colors.black87,
@@ -223,8 +352,7 @@ class MapPageController extends GetxController {
         _pciPolylines.add(tempPolylne);
       }
     }
-    polylines.clear();
-    polylines.addAll(_pciPolylines);
+    polylines.addAll(_pciPolylines.toSet());
   }
 
   // Function to animate the camera to the location of the polylines
@@ -263,7 +391,7 @@ class MapPageController extends GetxController {
     features = jsonData['features'];
 
     // Clear the polylines then add DRRP + PCI polylines
-    polylines.clear();
+    _pciPolylines.clear();
 
     for (int i = 0; i < features.length; i++) {
       List<LatLng> points = [];
@@ -289,9 +417,8 @@ class MapPageController extends GetxController {
         ],
       );
 
-      polylines.add(temporaryPolyline);
+      _pciPolylines.add(temporaryPolyline);
     }
-    polylines.addAll(_pciPolylines);
     _showCircularProgress.value = false;
     jsonData = {};
   }
