f7b6468204d94581def13e253fde9fcd406fd28e
Vikash <vk5418232@gmail.com>
2024-11-24 16:03:42 +0530

    added get pci based on velocity

--- lib/Functions/get_road_color.dart
+++ lib/Functions/get_road_color.dart
@@ -1,5 +1,4 @@
 // Function to get road color based on quality
-
 import 'package:flutter/material.dart';
 
 Color getRoadColor(double quality) {
@@ -16,5 +15,19 @@ Color getRoadColor(double quality) {
       return Colors.green;
     default:
       return Colors.black;
+  }
+}
+
+Color getVelocityColor(double velocity) {
+  if (velocity >= 30) {
+    return Colors.green;
+  } else if (velocity > 20) {
+    return Colors.blue;
+  } else if (velocity > 10) {
+    return Colors.yellow;
+  } else if (velocity > 5) {
+    return Colors.orange;
+  } else {
+    return Colors.red;
   }
 }
