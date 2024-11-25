9df0e5c6c1493f5e37aa5c94cb517c034e0c1e7c
Vikash <vk5418232@gmail.com>
2024-11-25 11:43:42 +0530

    lengens to show on map page

---
+++ lib/src/Presentation/Screens/MapsPage/widget/map_page_legends.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class Legends extends StatelessWidget {
+  const Legends({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        color: Colors.white54,
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 20,
+                height: 20,
+                color: Colors.red,
+              ),
+              const Gap(5),
+              const Text('PCI = 1'),
+            ],
+          ),
+          const Gap(5),
+          Row(
+            children: [
+              Container(
+                width: 20,
+                height: 20,
+                color: Colors.orange,
+              ),
+              const Gap(5),
+              const Text('PCI = 2'),
+            ],
+          ),
+          const Gap(5),
+          Row(
+            children: [
+              Container(
+                width: 20,
+                height: 20,
+                color: Colors.yellow,
+              ),
+              const Gap(5),
+              const Text('PCI = 3'),
+            ],
+          ),
+          const Gap(5),
+          Row(
+            children: [
+              Container(
+                width: 20,
+                height: 20,
+                color: Colors.blue,
+              ),
+              const Gap(5),
+              const Text('PCI = 4'),
+            ],
+          ),
+          const Gap(5),
+          Row(
+            children: [
+              Container(
+                width: 20,
+                height: 20,
+                color: Colors.green,
+              ),
+              const Gap(5),
+              const Text('PCI = 5'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
