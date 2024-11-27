23f20bf11dfcccd01852691fd47394c3fcb7f419
Vikash <vk5418232@gmail.com>
2024-11-27 17:21:03 +0530

    added multi g-plots functionality

--- lib/src/Presentation/Controllers/map_page_controller.dart
+++ lib/src/Presentation/Controllers/map_page_controller.dart
@@ -14,9 +14,11 @@ import '../../Models/stats_data.dart';
 class MapPageController extends GetxController {
   final RxBool _isDrrpLayerVisible = false.obs;
   final RxSet<Polyline> _pciPolylines = <Polyline>{}.obs;
-  List<Road> roads = <Road>[];
   GoogleMapController? _googleMapController;
-  List<Map<String, dynamic>> roadOutputDataQuery = <Map<String, dynamic>>[];
+  List<List<Map<String, dynamic>>> roadOutputData =
+      <List<Map<String, dynamic>>>[];
+  List<Map<String, dynamic>> selectedRoads = [];
+
   LatLng _minLat = const LatLng(0, 0);
   LatLng _maxLat = const LatLng(0, 0);
   List<RoadStats> roadStats = <RoadStats>[];
@@ -36,15 +38,11 @@ class MapPageController extends GetxController {
   final RxBool _showPCIlabel = true.obs;
   final RxBool _showIndicator = false.obs;
 
-  // variables to store the currently open road's metadata
-  Map<String, dynamic> currentRoad = <String, dynamic>{};
-
   // Getters
   bool get isDrrpLayerVisible => _isDrrpLayerVisible.value;
   RxSet<Polyline> get pciPolylines => _pciPolylines.toSet().obs;
   LatLng get getMinLat => _minLat;
   LatLng get getMaxLat => _maxLat;
-  List<Road> get getRoads => roads;
   List<RoadStats> get getRoadStats => roadStats;
   GoogleMapController? get getGoogleMapController => _googleMapController;
   List<MapType> get googlemapType => _googlemapType.toList();
@@ -58,7 +56,6 @@ class MapPageController extends GetxController {
   // Setters
   set isDrrpLayerVisible(bool value) => _isDrrpLayerVisible.value = value;
   set pciPolylines(Set<Polyline> value) => _pciPolylines.addAll(value);
-  set setRoads(List<Road> value) => roads.addAll(value);
   set setGoogleMapController(GoogleMapController? value) =>
       _googleMapController = value;
   set setMapType(List<String> value) => mapType.addAll(value);
@@ -71,6 +68,16 @@ class MapPageController extends GetxController {
 
   void clearPolylines() {
     _pciPolylines.clear();
+  }
+
+  void clearData() {
+    roadStats.clear();
+    _pciPolylines.clear();
+    roadOutputData.clear();
+    selectedRoads.clear();
+
+    _minLat = const LatLng(0, 0);
+    _maxLat = const LatLng(0, 0);
   }
 
   void setRoadStatistics(List<Map<String, dynamic>> query) {
@@ -111,84 +118,89 @@ class MapPageController extends GetxController {
 
   // fuction to plot the road data on the map
   Future<void> plotRoadData() async {
-    logger.d("plotting road data...");
-    roadStats.clear();
+    logger.i("${_pciPolylines.length}");
+    logger.i("${roadStats.length}");
     _pciPolylines.clear();
-    for (var road in roadOutputDataQuery) {
-      String roadName = road["roadName"];
-      List<dynamic> labels = jsonDecode(road["labels"]);
-      dynamic roadStatistics = jsonDecode(road["stats"]);
+    roadStats.clear();
+    logger.d("plotting road data...");
+    for (int i = 0; i < roadOutputData.length; i++) {
+      var roadQuery = roadOutputData[i];
+      for (int j = 0; j < roadQuery.length; j++) {
+        var road = roadQuery[j];
+        String roadName = road["roadName"];
+        List<dynamic> labels = jsonDecode(road["labels"]);
+        dynamic roadStatistics = jsonDecode(road["stats"]);
 
-      _minLat = (_minLat.latitude == 0)
-          ? LatLng(labels[0]['latitude'], labels[0]['longitude'])
-          : _minLat;
+        _minLat = (_minLat.latitude == 0)
+            ? LatLng(labels[0]['latitude'], labels[0]['longitude'])
+            : _minLat;
 
-      // Adding the stats
-      List<RoadStatsData> roadStatsList = [];
-      for (int i = 1; i <= 5; i++) {
-        // Key represents each PCI value in the stats, which are only 1-5
-        String key = "$i";
-        roadStatsList.add(
-          RoadStatsData(
-            pci: key,
-            avgVelocity: roadStatistics[key]['avg_velocity'].toString(),
-            distanceTravelled:
-                roadStatistics[key]['distance_travelled'].toString(),
-            numberOfSegments:
-                roadStatistics[key]['number_of_segments'].toString(),
+        // Adding the stats
+        List<RoadStatsData> roadStatsList = [];
+        for (int i = 1; i <= 5; i++) {
+          // Key represents each PCI value in the stats, which are only 1-5
+          String key = "$i";
+          roadStatsList.add(
+            RoadStatsData(
+              pci: key,
+              avgVelocity: roadStatistics[key]['avg_velocity'].toString(),
+              distanceTravelled:
+                  roadStatistics[key]['distance_travelled'].toString(),
+              numberOfSegments:
+                  roadStatistics[key]['number_of_segments'].toString(),
+            ),
+          );
+        }
+        roadStats.add(
+          RoadStats(
+            roadName: roadName,
+            roadStatsData: roadStatsList,
           ),
         );
-      }
-
-      roadStats.add(
-        RoadStats(
-          roadName: roadName,
-          roadStatsData: roadStatsList,
-        ),
-      );
-
-      // Adding the labels and polylines
-      for (int i = 1; i < labels.length; i++) {
-        _maxLat = LatLng(labels[i]['latitude'], labels[i]['longitude']);
-        double avgVelocity =
-            (labels[i]['velocity'] + labels[i - 1]['velocity']) / 2;
-        avgVelocity *= 3.6; // convert m/s to kmph
-        double prediction = max(
-            double.parse(labels[i]['prediction'].toString()),
-            double.parse(labels[i - 1]['prediction'].toString()));
-        double velocityPCI = velocityToPCI(avgVelocity);
-        // Add the polyline
-        Polyline tempPolylne = Polyline(
-          consumeTapEvents: true,
-          polylineId: PolylineId("$roadName$i"),
-          color: _showPCIlabel.value
-              ? getRoadColor(prediction)
-              : getVelocityColor(avgVelocity),
-          width: 5,
-          endCap: Cap.roundCap,
-          startCap: Cap.roundCap,
-          jointType: JointType.round,
-          points: [
-            LatLng(labels[i - 1]['latitude'], labels[i - 1]['longitude']),
-            LatLng(labels[i]['latitude'], labels[i]['longitude']),
-          ],
-          onTap: () {
-            Map<String, dynamic> polylineOnTapData = {
-              'roadName': roadName,
-              'filename': currentRoad['filename'],
-              'time': currentRoad['time'],
-              'pci_pred': prediction,
-              'vel_pred': velocityPCI,
-              'avg_vel': avgVelocity,
-            };
-            Get.bottomSheet(
-              clipBehavior: Clip.antiAlias,
-              backgroundColor: Colors.white,
-              PolylineBottomSheet(data: polylineOnTapData),
-            );
-          },
-        );
-        _pciPolylines.add(tempPolylne);
+        // Adding the labels and polylines
+        for (int k = 1; k < labels.length; k++) {
+          _maxLat = LatLng(labels[k]['latitude'], labels[k]['longitude']);
+          double avgVelocity =
+              (labels[k]['velocity'] + labels[k - 1]['velocity']) / 2;
+          avgVelocity *= 3.6; // convert m/s to kmph
+          double prediction = max(
+              double.parse(labels[k]['prediction'].toString()),
+              double.parse(labels[k - 1]['prediction'].toString()));
+          double velocityPCI = velocityToPCI(avgVelocity);
+          var currentRoad = selectedRoads[i];
+          // Add the polyline
+          Polyline tempPolylne = Polyline(
+            consumeTapEvents: true,
+            polylineId: PolylineId("$roadName$i$j$k"),
+            color: _showPCIlabel.value
+                ? getRoadColor(prediction)
+                : getVelocityColor(avgVelocity),
+            width: 5,
+            endCap: Cap.roundCap,
+            startCap: Cap.roundCap,
+            jointType: JointType.round,
+            points: [
+              LatLng(labels[k - 1]['latitude'], labels[k - 1]['longitude']),
+              LatLng(labels[k]['latitude'], labels[k]['longitude']),
+            ],
+            onTap: () {
+              Map<String, dynamic> polylineOnTapData = {
+                'roadName': roadName,
+                'filename': currentRoad['filename'],
+                'time': currentRoad['time'],
+                'pci_pred': prediction,
+                'vel_pred': velocityPCI,
+                'avg_vel': avgVelocity,
+              };
+              Get.bottomSheet(
+                clipBehavior: Clip.antiAlias,
+                backgroundColor: Colors.white,
+                PolylineBottomSheet(data: polylineOnTapData),
+              );
+            },
+          );
+          _pciPolylines.add(tempPolylne);
+        }
       }
     }
   }
@@ -203,15 +215,12 @@ class MapPageController extends GetxController {
   // This function is used to plot the DRRP layer on the map.
   // It reads the geojson file containing the DRRP data and plots the polylines on the map.
   Future<void> plotDRRPLayer() async {
-    _showCircularProgress.value = true;
     Map<String, dynamic> jsonData = {};
     List<dynamic> features = [];
     String geoJsonString = await rootBundle.loadString(assetsPath.roadDRRP);
 
     jsonData = jsonDecode(geoJsonString);
     features = jsonData['features'];
-
-    _pciPolylines.clear();
 
     for (int i = 0; i < features.length; i++) {
       List<LatLng> points = [];
@@ -238,6 +247,5 @@ class MapPageController extends GetxController {
       );
       _pciPolylines.add(tempPolyline);
     }
-    _showCircularProgress.value = false;
   }
 }
