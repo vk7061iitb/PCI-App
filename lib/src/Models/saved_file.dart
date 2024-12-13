ab115ae683d785bf6210a381ee76f2f90e6a45bb
Vikash <vk5418232@gmail.com>
2024-12-13 07:29:02 +0530

    model for saved file metadata

---
+++ lib/src/Models/saved_file.dart
@@ -0,0 +1,17 @@
+class SavedFile {
+  final String filename;
+  final String time;
+  final String vehicleType;
+  final String path;
+  final String planned;
+  final int status;
+
+  SavedFile({
+    required this.filename,
+    required this.time,
+    required this.vehicleType,
+    required this.path,
+    required this.planned,
+    required this.status,
+  });
+}
