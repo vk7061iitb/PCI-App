c3a7e2046fb415ffcd516b342c28e1bec00dd383
Vikash <vk5418232@gmail.com>
2025-01-15 13:09:22 +0530

    updated the function

--- lib/Utils/vel_to_pci.dart
+++ lib/Utils/vel_to_pci.dart
@@ -1,11 +1,14 @@
-double velocityToPCI(double velocity) {
-  if (velocity >= 30) {
+/// calculates the corresponding PCI for a given velocity
+double velocityToPCI({
+  required double velocityKmph,
+}) {
+  if (velocityKmph >= 39) {
     return 5;
-  } else if (velocity > 20) {
+  } else if (velocityKmph > 30) {
     return 4;
-  } else if (velocity > 10) {
+  } else if (velocityKmph > 20) {
     return 3;
-  } else if (velocity > 5) {
+  } else if (velocityKmph > 10) {
     return 2;
   } else {
     return 1;
