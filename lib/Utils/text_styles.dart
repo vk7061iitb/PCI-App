5fa2d434192e6b9b73e3e2b13e39393953b5b817
Vikash <vk5418232@gmail.com>
2025-05-25 10:04:33 +0530

    textstyle used in the app

---
+++ lib/Utils/text_styles.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pciapp/Objects/data.dart';
+
+TextStyle dialogTitleStyle = GoogleFonts.inter(
+  color: textColor,
+  fontWeight: FontWeight.w500,
+  fontSize: 24,
+);
+
+TextStyle dialogContentStyle = GoogleFonts.inter(
+  color: textColor,
+  fontWeight: FontWeight.w400,
+  fontSize: 16,
+);
+
+TextStyle dialogButtonStyle = GoogleFonts.inter(
+  color: activeColor,
+  fontWeight: FontWeight.w600,
+  fontSize: 16,
+);
+
