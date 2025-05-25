7d4b0634dc082a49b2316ffdb75c7e23a4fe8321
Vikash <vk5418232@gmail.com>
2025-05-25 09:36:08 +0530

    simplified the widget added map legend

--- lib/src/Presentation/Screens/MapsPage/widget/map_page_legends.dart
+++ lib/src/Presentation/Screens/MapsPage/widget/map_page_legends.dart
@@ -1,81 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
-import '../../../../../Objects/data.dart';
 
-class Legends extends StatelessWidget {
-  const Legends({super.key});
+Map<String, Color> legend = {
+  "1": Colors.red,
+  "2": Colors.orange,
+  "3": Colors.yellow,
+  "4": Colors.blue,
+  "5": Colors.green,
+};
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(5),
-      decoration: BoxDecoration(
-        color: backgroundColor,
-        boxShadow: const [],
-      ),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                width: 20,
-                height: 20,
-                color: Colors.red,
-              ),
-              const Gap(5),
-              const Text('PCI = 1'),
-            ],
-          ),
-          const Gap(5),
-          Row(
-            children: [
-              Container(
-                width: 20,
-                height: 20,
-                color: Colors.orange,
-              ),
-              const Gap(5),
-              const Text('PCI = 2'),
-            ],
-          ),
-          const Gap(5),
-          Row(
-            children: [
-              Container(
-                width: 20,
-                height: 20,
-                color: Colors.yellow,
-              ),
-              const Gap(5),
-              const Text('PCI = 3'),
-            ],
-          ),
-          const Gap(5),
-          Row(
-            children: [
-              Container(
-                width: 20,
-                height: 20,
-                color: Colors.blue,
-              ),
-              const Gap(5),
-              const Text('PCI = 4'),
-            ],
-          ),
-          const Gap(5),
-          Row(
-            children: [
-              Container(
-                width: 20,
-                height: 20,
-                color: Colors.green,
-              ),
-              const Gap(5),
-              const Text('PCI = 5'),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
